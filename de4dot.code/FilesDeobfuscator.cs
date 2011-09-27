157a125894abf304276f354a6840158182448be6
de4dot <de4dot@gmail.com>
2011-09-28 01:44:32 +0200

    Catch all exceptions and print warning if load fails

--- de4dot.code/FilesDeobfuscator.cs
+++ de4dot.code/FilesDeobfuscator.cs
@@ -167,8 +167,9 @@ namespace de4dot {
 				catch (BadImageFormatException) {
 					return;		// Not a .NET file
 				}
-				catch (IOException) {
-					Log.w("Could not load file: {0}", file.Filename);
+				catch (Exception ex) {
+					Log.w("Could not load file: {0}. Use -v to see stack trace.", file.Filename);
+					Utils.printStackTrace(ex, Log.LogLevel.verbose);
 					return;
 				}
 
