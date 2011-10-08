9ade539ecd30bb8653effaa0590b3c1e317925f1
de4dot <de4dot@gmail.com>
2011-10-08 20:03:10 +0200

    Update version to 1.0.3

--- de4dot.code/Properties/AssemblyInfo.cs
+++ de4dot.code/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.2.3405")]
-[assembly: AssemblyFileVersion("1.0.2.3405")]
+[assembly: AssemblyVersion("1.0.3.3405")]
+[assembly: AssemblyFileVersion("1.0.3.3405")]
