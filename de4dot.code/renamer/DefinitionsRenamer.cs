a23a889776e6722d50fe083ccaf01903727a6163
de4dot <de4dot@gmail.com>
2011-11-05 08:08:16 +0100

    Ignore resolve errors. It's likely an obfuscator bug.

--- de4dot.code/renamer/DefinitionsRenamer.cs
+++ de4dot.code/renamer/DefinitionsRenamer.cs
@@ -504,7 +504,8 @@ namespace de4dot.renamer {
 			}
 			if (isAutoCreatedType(typeReference))
 				return null;
-			throw new ApplicationException(string.Format("Could not resolve TypeReference {0} ({1:X8})", typeReference, typeReference.MetadataToken.ToInt32()));
+			Log.e("Could not resolve TypeReference {0} ({1:X8})", typeReference, typeReference.MetadataToken.ToInt32());
+			return null;
 		}
 
 		public MethodDef resolve(MethodReference methodReference) {
@@ -520,7 +521,8 @@ namespace de4dot.renamer {
 			}
 			if (isAutoCreatedType(methodReference.DeclaringType))
 				return null;
-			throw new ApplicationException(string.Format("Could not resolve MethodReference {0} ({1:X8})", methodReference, methodReference.MetadataToken.ToInt32()));
+			Log.e("Could not resolve MethodReference {0} ({1:X8})", methodReference, methodReference.MetadataToken.ToInt32());
+			return null;
 		}
 
 		public FieldDef resolve(FieldReference fieldReference) {
@@ -536,7 +538,8 @@ namespace de4dot.renamer {
 			}
 			if (isAutoCreatedType(fieldReference.DeclaringType))
 				return null;
-			throw new ApplicationException(string.Format("Could not resolve FieldReference {0} ({1:X8})", fieldReference, fieldReference.MetadataToken.ToInt32()));
+			Log.e("Could not resolve FieldReference {0} ({1:X8})", fieldReference, fieldReference.MetadataToken.ToInt32());
+			return null;
 		}
 
 		public MethodDef findMethod(MethodReference methodReference) {
