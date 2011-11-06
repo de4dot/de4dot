a369d36553d4211a6b0a6c7adb1006c5696f0d83
de4dot <de4dot@gmail.com>
2011-11-06 12:14:16 +0100

    Add compare() byte[] method

--- de4dot.code/Utils.cs
+++ de4dot.code/Utils.cs
@@ -216,5 +216,15 @@ namespace de4dot {
 				return false;
 			}
 		}
+
+		public static bool compare(byte[] a, byte[] b) {
+			if (a.Length != b.Length)
+				return false;
+			for (int i = 0; i < a.Length; i++) {
+				if (a[i] != b[i])
+					return false;
+			}
+			return true;
+		}
 	}
 }
