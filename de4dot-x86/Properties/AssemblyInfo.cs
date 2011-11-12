6bf3de0deeee0cad9a05489f05d95d466531752f
de4dot <de4dot@gmail.com>
2011-11-12 16:57:00 +0100

    Set version: 1.2.2

--- de4dot-x86/Properties/AssemblyInfo.cs
+++ de4dot-x86/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.1.3405")]
-[assembly: AssemblyFileVersion("1.2.1.3405")]
+[assembly: AssemblyVersion("1.2.2.3405")]
+[assembly: AssemblyFileVersion("1.2.2.3405")]
