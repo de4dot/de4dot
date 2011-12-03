7a6af408320a88db3da8d991ef2b620bc6d2bf1e
de4dot <de4dot@gmail.com>
2011-12-03 14:33:37 +0100

    Update version: 1.3.4

--- blocks/Properties/AssemblyInfo.cs
+++ blocks/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.3.3.3405")]
-[assembly: AssemblyFileVersion("1.3.3.3405")]
+[assembly: AssemblyVersion("1.3.4.3405")]
+[assembly: AssemblyFileVersion("1.3.4.3405")]
