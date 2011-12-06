5b7806cc1f030d9f3e5199f14305c36ebbf5635a
de4dot <de4dot@gmail.com>
2011-12-06 17:38:30 +0100

    New version: 1.3.6

--- AssemblyServer/Properties/AssemblyInfo.cs
+++ AssemblyServer/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.3.5.3405")]
-[assembly: AssemblyFileVersion("1.3.5.3405")]
+[assembly: AssemblyVersion("1.3.6.3405")]
+[assembly: AssemblyFileVersion("1.3.6.3405")]
