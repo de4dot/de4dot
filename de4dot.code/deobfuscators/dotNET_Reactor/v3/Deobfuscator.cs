1a1350410aa3517df704f9a28e114f5e5bdcbd4c
de4dot <de4dot@gmail.com>
2011-12-22 18:49:22 +0100

    Only call patcher if we need to patch it

--- de4dot.code/deobfuscators/dotNET_Reactor/v3/Deobfuscator.cs
+++ de4dot.code/deobfuscators/dotNET_Reactor/v3/Deobfuscator.cs
@@ -136,8 +136,12 @@ namespace de4dot.code.deobfuscators.dotNET_Reactor.v3 {
 			return data;
 		}
 
+		bool needsPatching() {
+			return decrypterType.LinkedResource != null || nativeLibSaver.Resource != null;
+		}
+
 		public override bool getDecryptedModule(ref byte[] newFileData, ref Dictionary<uint, DumpedMethod> dumpedMethods) {
-			if (!decrypterType.Detected)
+			if (!needsPatching())
 				return false;
 
 			var fileData = ModuleBytes ?? DeobUtils.readModule(module);
