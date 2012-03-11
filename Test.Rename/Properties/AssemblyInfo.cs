8c5c055066cec1652fa6dd68e3bc8ad8e2151b98
de4dot <de4dot@gmail.com>
2012-03-11 15:35:55 +0100

    New version: 1.7.2

--- Test.Rename/Properties/AssemblyInfo.cs
+++ Test.Rename/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.7.1.3405")]
-[assembly: AssemblyFileVersion("1.7.1.3405")]
+[assembly: AssemblyVersion("1.7.2.3405")]
+[assembly: AssemblyFileVersion("1.7.2.3405")]
