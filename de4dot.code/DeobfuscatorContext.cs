88d7607d10b89d0a024a381a8eeb9054f4744300
de4dot <de4dot@gmail.com>
2012-04-24 11:25:39 +0200

    Fix resolver

--- de4dot.code/DeobfuscatorContext.cs
+++ de4dot.code/DeobfuscatorContext.cs
@@ -48,10 +48,19 @@ namespace de4dot.code {
 			dataDict.Remove(name);
 		}
 
+		static TypeReference getNonGenericTypeReference(TypeReference typeReference) {
+			if (typeReference == null)
+				return null;
+			if (!typeReference.IsGenericInstance)
+				return typeReference;
+			var type = (GenericInstanceType)typeReference;
+			return type.ElementType;
+		}
+
 		public TypeDefinition resolve(TypeReference type) {
 			if (type == null)
 				return null;
-			var typeDef = type as TypeDefinition;
+			var typeDef = getNonGenericTypeReference(type) as TypeDefinition;
 			if (typeDef != null)
 				return typeDef;
 
