3646bca56b4490cdb22f2a769f9d726d3bfde1b3
de4dot <de4dot@gmail.com>
2012-11-11 16:56:29 +0100

    Align the numbers

--- de4dot.code/deobfuscators/MaxtoCode/CryptDecrypter.cs
+++ de4dot.code/deobfuscators/MaxtoCode/CryptDecrypter.cs
@@ -93,7 +93,7 @@ namespace de4dot.code.deobfuscators.MaxtoCode {
 			16,  7, 20, 21, 29, 12, 28, 17,
 			 1, 15, 23, 26,  5, 18, 31, 10,
 			 2,  8, 24, 14, 32, 27,  3,  9,
-			 19, 13, 30, 6, 22, 11, 4, 25,
+			19, 13, 30,  6, 22, 11,  4, 25,
 		};
 		static readonly byte[] esel = new byte[48] {
 			32,  1,  2,  3,  4,  5,  4,  5,
