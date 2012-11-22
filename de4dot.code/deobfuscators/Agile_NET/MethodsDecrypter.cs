3a519b51d8d1257b01a4919283b25e55a2ef1367
de4dot <de4dot@gmail.com>
2012-11-22 05:50:05 +0100

    This shouldn't be a warning

--- de4dot.code/deobfuscators/Agile_NET/MethodsDecrypter.cs
+++ de4dot.code/deobfuscators/Agile_NET/MethodsDecrypter.cs
@@ -447,7 +447,7 @@ namespace de4dot.code.deobfuscators.Agile_NET {
 			case DecryptResult.NotEncrypted: return false;
 
 			case DecryptResult.Error:
-				Logger.w("Using dynamic method decryption");
+				Logger.n("Using dynamic method decryption");
 				byte[] moduleCctorBytes = getModuleCctorBytes(csRtType);
 				dumpedMethods = de4dot.code.deobfuscators.MethodsDecrypter.decrypt(module.Location, moduleCctorBytes);
 				return true;
