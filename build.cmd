19d96072f5388eff1998ddaeaa5dcbe0bf7eb88e
de4dot <de4dot@gmail.com>
2019-09-26 21:41:04 +0200

    Update build scripts and projects

--- build.cmd
+++ build.cmd
@@ -4,13 +4,10 @@ msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net35
 del Release\net35\*.pdb Release\net35\*.xml Release\net35\Test.Rename.* || goto :error
 
 dotnet publish -c Release -f netcoreapp2.1 -o publish-netcoreapp2.1 de4dot || goto :error
-del publish-netcoreapp2.1\*.pdb || goto :error
-del publish-netcoreapp2.1\*.xml || goto :error
-dotnet publish -c Release -f netcoreapp3.0 -o publish-netcoreapp3.0 de4dot || goto :error
-del publish-netcoreapp3.0\*.pdb || goto :error
-del publish-netcoreapp3.0\*.xml || goto :error
+del publish-netcoreapp2.1\*.pdb publish-netcoreapp2.1\*.xml || goto :error
 
-exit /b 0
+dotnet publish -c Release -f netcoreapp3.0 -o publish-netcoreapp3.0 de4dot || goto :error
+del publish-netcoreapp3.0\*.pdb publish-netcoreapp3.0\*.xml || goto :error
 
 goto :EOF
 
