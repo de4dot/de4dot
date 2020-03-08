078beebbd58798f6b0eb328c56ef003fce38aa70
0xd4d <de4dot@gmail.com>
2020-03-08 18:29:32 +0100

    netcoreapp3.0 -> netcoreapp3.1

--- build.ps1
+++ build.ps1
@@ -16,6 +16,6 @@ dotnet publish -c Release -f netcoreapp2.1 -o publish-netcoreapp2.1 de4dot
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 Remove-Item publish-netcoreapp2.1\*.pdb, publish-netcoreapp2.1\*.xml
 
-dotnet publish -c Release -f netcoreapp3.0 -o publish-netcoreapp3.0 de4dot
+dotnet publish -c Release -f netcoreapp3.1 -o publish-netcoreapp3.1 de4dot
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
-Remove-Item publish-netcoreapp3.0\*.pdb, publish-netcoreapp3.0\*.xml
+Remove-Item publish-netcoreapp3.1\*.pdb, publish-netcoreapp3.1\*.xml
